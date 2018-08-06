59e9ff3fd3de11f677edc742394b950065c09884
GeneralBlue <generalblue@VADER-666>
2018-08-06 16:16:55 +0200

    Voy a dejarlo, por el momento...

---
+++ SERVER/dbconfig.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * Database connection data 
+ */
+$host = "sql205.unaux.com";
+$user = "unaux_22506538";
+$password = "esmegma#88"; /* ERASE BEFORE COMMIT */
+$database = "unaux_22506538_3multip";
+//$port = "";
+//$socket = "";
+
+/*
+ * Database structure 
+ */
+/* Player names table */
+$namesTable = "playernames";
+$idAttr = "rowid";
+$nameAttr = "name"; // VARCHAR 16
+
+/* Move table */
+$moveTable = "movements";
+$idAttrMove = "rowid";
+$playerMove = "player";
+$moveAttr = "move";
+$turnAttr = "turn";
