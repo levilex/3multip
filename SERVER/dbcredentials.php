981439995dba4790744b095b626eab54dc7275b1
Ricard Barnes <29869854+ricardbarnes@users.noreply.github.com>
2018-11-13 20:20:23 +0100

    Add files via upload

---
+++ SERVER/dbcredentials.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * Database connection data --> En un proyecto serio, esto no habría que "commitearlo"
+ */
+$host = "sql205.unaux.com";
+$user = "unaux_22506538";
+$password = "esmegma#88"; /* ERASE BEFORE COMMIT */
+$database = "unaux_22506538_3multip";
+//$port = "";
+//$socket = "";
